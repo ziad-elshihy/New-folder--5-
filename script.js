be0ee7d55de166e77cb6002c7eebedb516e541a4
ziad-elshihy <elshihyziad@gmail.com>
2023-09-10 12:27:23 +0300

    .....

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-
-window.onload = function () {
-
-   const prevBtn = document.getElementById('prev');
-   const nextBtn = document.getElementById('next');
-
-   prevBtn.addEventListener("click", function () {
-      prevSlide();
-   });
-   nextBtn.addEventListener("click", function () {
-      nextSlide();
-   });
-
+function NavBg() {
+   let nav = document.querySelector('#nav')
+   let navLink = document.querySelectorAll('#nav-link')
+   let scrollValue = window.scrollY
+   navLink.forEach(link => {
+   if (scrollValue < 100) {
+      nav.classList.remove('bgColor')
+      link.classList.remove('bgColor')
+   } else {
+      nav.classList.add('bgColor')
+      link.classList.add('bgColor')
+      ;
+   }})
 }
-let slideNumber = 0;
-const prevSlide = () => {
-   const slides = document.getElementsByClassName('slides');
-   const slider = document.getElementById("slider");
-   const currentSlide = slider.getElementsByClassName('current');
-   currentSlide[0].classList.remove("current");
-   if (slideNumber == 0) {
-      slideNumber = slides.length - 1;
-   }
-   else {
-      slideNumber = slideNumber - 1;
-   }
-   slides[slideNumber].classList.add("current");
-}
-const nextSlide = () => {
-   const slides = document.getElementsByClassName('slides');
-   const slider = document.getElementById("slider");
-   const currentSlide = slider.getElementsByClassName('current');
-   currentSlide[0].classList.remove("current");
-   if (slideNumber == (slides.length - 1)) {
-      slideNumber = 0;
-   }
-   else {
-      slideNumber = slideNumber + 1;
-   }
-   slides[slideNumber].classList.add("current");
-}
+window.addEventListener('scroll' , NavBg)
